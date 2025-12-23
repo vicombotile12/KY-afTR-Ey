1447e48bf262fac4bad7f1a16e26f04e1fdb5d85
vicombotile12 <ajibawomoyinoluwa6@gmail.com>
2025-12-23 22:38:00 +0100

    little things

--- Chesty.gd
+++ Chesty.gd
@@ -1,15 +1,16 @@
 extends CharacterBody2D
 
-const accel_grav = 5
-var vector_grav = Vector2(0,1)
-var gravity = accel_grav * vector_grav
+const accel_grav = 980
+const v_gravs = [Vector2(0,1), Vector2(1,0), Vector2(0,-1), Vector2(-1,0)]
+var v_grav = v_gravs[0]
+var gravity = accel_grav * v_grav
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass # Replace with code
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
+	velocity += gravity * delta
 	move_and_slide()
