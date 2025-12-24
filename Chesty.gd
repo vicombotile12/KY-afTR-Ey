3f2cdcf18a2f55a491fdb8e16f3451e74cd43908
vicombotile12 <ajibawomoyinoluwa6@gmail.com>
2025-12-24 09:29:53 +0100

    Update Chesty.gd

--- Chesty.gd
+++ Chesty.gd
@@ -7,10 +7,15 @@ var gravity = accel_grav * v_grav
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with code
-
-
+	pass # Replace with function 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
+func _physics_process(delta: float)-> void:
+	# first unlocked key
+	if Input.is_action_just_pressed("rightkey"):
+		match v_grav: #should hopefully scroll through gravities
+			Vector2(0,1): v_grav = v_gravs[1]
+			Vector2(1,0): v_grav = v_gravs[2]
+			Vector2(0,-1): v_grav = v_gravs[3]
+			Vector2(-1,0): v_grav = v_gravs[0]
 	velocity += gravity * delta
 	move_and_slide()
