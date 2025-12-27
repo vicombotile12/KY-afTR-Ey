008f664bbc160deafe572ac46da3995e97d24e07
vicombotile12 <ajibawomoyinoluwa6@gmail.com>
2025-12-27 12:23:29 +0100

    Chesty physics engine
    yeah i added wall friction + straightness, i'm now getting started on the level changing system. NO PROCRASTINATING, MYSELF!

--- scripts/Chesty.gd
+++ scripts/Chesty.gd
@@ -7,10 +7,12 @@ var gravity = accel_grav * v_grav
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function 
+	pass
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float)-> void:
 	# first unlocked key
+	
 	if Input.is_action_just_pressed("rightkey") and (is_on_floor() or is_on_wall()): # can only change gravity under those conditions
 		match v_grav: #should hopefully scroll through gravities
 			Vector2.DOWN: v_grav = v_gravs[1]; up_direction = Vector2.LEFT
@@ -19,6 +21,12 @@ func _physics_process(delta: float)-> void:
 			Vector2.LEFT: v_grav = v_gravs[0]; up_direction = Vector2.UP
 		gravity = v_grav * accel_grav
 		$Sprite2D.rotation_degrees += -90
-	velocity += gravity * delta
+	
+	if is_on_wall() and (gravity.y > 0.65 or gravity.x > 0.65):
+		gravity -= 0.03 * v_grav * accel_grav #wall friction
+	elif not is_on_wall():
+		gravity = v_grav * accel_grav
+	
+	velocity = (gravity * delta + velocity) /1.025 # <- that was to help with straightness of velocity relative to gravity
 	move_and_slide()
 	# got it to work :]
--- scripts/nextlevel.gd
+++ scripts/nextlevel.gd
@@ -1,16 +1,15 @@
-extends Node
-signal change_room
-
-var Activated =  false
+extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	var sceneroot = get_node("./")
+	#sceneroot.change_room.connect(_change_room)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func _change_room(number: int) -> void:
-	pass
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		pass#emit_signal()
