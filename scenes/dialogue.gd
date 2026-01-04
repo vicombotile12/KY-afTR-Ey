1e633435bb7e35cbb5dacfba1fbb20d9d464a789
vicombotile12 <ajibawomoyinoluwa6@gmail.com>
2026-01-04 21:16:46 +0100

    added music tried adding tutorial

--- scenes/dialogue.gd
+++ scenes/dialogue.gd
@@ -11,4 +11,8 @@ func _process(delta: float) -> void:
 
 func down():
 	$AnimationPlayer.play("Down")
-	queue_free()
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "Down":
+		queue_free()
